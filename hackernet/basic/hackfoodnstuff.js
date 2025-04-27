08523212ee8c875ad5d070e702890b1f6f424209
hongjiNH <40945967+hongjiNH@users.noreply.github.com>
2025-04-27 22:20:14 +0800

    Create hackfoodnstuff.js

---
+++ hackernet/basic/hackfoodnstuff.js
@@ -0,0 +1,18 @@
+/** @param {NS} ns */
+export async function main(ns) {
+  const target = "foodnstuff";
+
+  if (!ns.hasRootAccess(target)) {
+    ns.tprint(`[🛡️] No root access on ${target}. Attempting to nuke...`);
+    try {
+      ns.nuke(target);
+    } catch (e) {
+      ns.tprint("[❌] Failed to nuke. You probably don't have NUKE.exe.");
+      return;
+    }
+  }
+
+  while (true) {
+    await ns.hack(target);
+  }
+}
